7fee68f64e0d64f7bdf816f37d75f4c67201115d
anastasia <anastasia.aizman@gmail.com>
2017-05-09 15:12:46 -0400

    bump - setup

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname+'.md')).read()
 
 setup(
-    name = "minhash",
-    version = "0.0.1",
-    author = "Anastasia Aizman",
-    author_email = "anastasia.aizman@gmail.com",
-    description = ("minhash"),
-    keywords = "minhash",
+    name="minhash",
+    version="0.0.1",
+    author="Anastasia Aizman",
+    author_email="anastasia.aizman@gmail.com",
+    description=("minhash"),
+    keywords="minhash",
     py_modules=["minhash"],
-    url = "",
+    url="",
     long_description=read("README"),
 )
