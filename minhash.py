c939fa04c5c32564f26851848b1858e17ea50e24
anastasia <anastasia.aizman@gmail.com>
2017-05-09 15:12:32 -0400

    update - allowing user to change settings

--- minhash.py
+++ minhash.py
@@ -3,32 +3,42 @@ import random
 import binascii
 
 HASH_NUM = 1000
+
+# 32 byte hash
 MAX_SHINGLE_ID = 2**32-1
 
 # prime num is the next prime greater than MAX_SHINGLE_ID
 PRIME = 4294967311
+
 SHINGLE_SIZE = 3
+
 # SHINGLE_TYPE is 'word' or 'char'
 SHINGLE_TYPE = 'word'
 
+def calculate(s1, s2, coeffs_a=None, coeffs_b=None, total_hash_num=None, max_shingle_id=None, shingle_size=None, shingle_type=None):
+    if type(s1) == str:
+        # if string, turn to shingles
+        shingles1 = str_to_shingles(s1, shingle_size=shingle_size, shingle_type=shingle_type)
+        shingles2 = str_to_shingles(s2, shingle_size=shingle_size, shingle_type=shingle_type)
 
-def calculate(str_one, str_two):
-    shingles_one = str_to_shingles(str_one)
-    shingles_two = str_to_shingles(str_two)
+    elif type(s1) == list or type(s1) == set:
+        # assuming shingles
+        shingles1 = s1
+        shingles2 = s2
 
-    coeffs_a = generate_coefficients()
-    coeffs_b = generate_coefficients()
+    if not coeffs_a and not coeffs_b:
+        coeffs_a = generate_coefficients(total_hash_num=total_hash_num, max_shingle_id=max_shingle_id)
+        coeffs_b = generate_coefficients(total_hash_num=total_hash_num, max_shingle_id=max_shingle_id)
 
-    sigs_a = get_min_signatures(shingles_one, coeffs_a, coeffs_b)
-    sigs_b = get_min_signatures(shingles_two, coeffs_a, coeffs_b)
+    sigs_a = get_min_signatures(shingles1, coeffs_a, coeffs_b)
+    sigs_b = get_min_signatures(shingles2, coeffs_a, coeffs_b)
 
-    # union_count has to be float so that our minhash doesn't get cast to int
-    union_count = 0.0
+    union_count = 0
     for i, val in enumerate(sigs_a):
         if sigs_b[i] == val:
             union_count += 1
 
-    return union_count / HASH_NUM
+    return union_count / float(HASH_NUM)
 
 def get_min_signatures(shingles, coeffs_a, coeffs_b):
     min_signatures = list()
@@ -45,21 +55,27 @@ def get_min_signatures(shingles, coeffs_a, coeffs_b):
         hash_count += 1
     return min_signatures
 
-def str_to_shingles(string):
+def str_to_shingles(string, shingle_size=None, shingle_type=None):
     shingles_in_doc = set()
 
-    if SHINGLE_TYPE == 'word':
+    if not shingle_size:
+        shingle_size = SHINGLE_SIZE
+
+    if not shingle_type:
+        shingle_type = SHINGLE_TYPE
+
+    if shingle_type == 'word':
         units = string.split(' ')
-    elif SHINGLE_TYPE == 'char':
+    elif shingle_type == 'char':
         units = list(string)
 
-    for idx in range(0, len(units) - (SHINGLE_SIZE - 1)):
-        shingle = create_shingle(units[idx:idx+SHINGLE_SIZE])
+    for idx in range(0, len(units) - (shingle_size - 1)):
+        shingle = create_hashed_shingle(units[idx:idx+shingle_size])
         shingles_in_doc.add(shingle)
 
     return shingles_in_doc
 
-def create_shingle(list_to_create):
+def create_hashed_shingle(list_to_create):
     if SHINGLE_TYPE == 'word':
         shingle = ' '.join(list_to_create)
 
@@ -69,21 +85,27 @@ def create_shingle(list_to_create):
     # hash the shingle to a 32-bit integer
     return binascii.crc32(shingle) & 0xffffffff
 
-def generate_coefficients():
-  # create a unique set of 'HASH_NUM' random values
-  rand_set = set()
-  hash_num = 0
+def generate_coefficients(total_hash_num=None, max_shingle_id=None):
+    # create a unique set of 'HASH_NUM' random values
+    rand_set = set()
+    hash_num = 0
 
-  while hash_num < HASH_NUM:
-    rand_num = random.randint(0, MAX_SHINGLE_ID)
-    rand_set.add(rand_num)
+    if not total_hash_num:
+        total_hash_num = HASH_NUM
 
-    # if rand_num not added, it means that it already exists in set
-    # Try again.
-    if len(rand_set) - 1 == hash_num:
-        hash_num += 1
+    if not max_shingle_id:
+        max_shingle_id = MAX_SHINGLE_ID
 
-  return list(rand_set)
+    while hash_num < total_hash_num:
+        rand_num = random.randint(0, max_shingle_id)
+        rand_set.add(rand_num)
+
+        # if rand_num not added, it means that it already exists in set
+        # Try again.
+        if len(rand_set) - 1 == hash_num:
+            hash_num += 1
+
+    return list(rand_set)
 
 if __name__ == '__main__':
     str_one, str_two = ['','']
